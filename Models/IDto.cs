8202d325bf91b5e4ca3efef2a660f24ea192c465
jspauldo98 <jaredspaulding@spauldoindustries.com>
2023-02-02 12:17:13 -0700

    Brought dto id decode into backend process:

--- Models/IDto.cs
+++ Models/IDto.cs
@@ -5,6 +5,7 @@ namespace spauldo_tecture.Models
     public interface IDto
     {
         string Id { get; }
+        int? Decode(IHashids hashids);
         IModel MapToModel(IHashids hashids);
     }
 }
--- Models/SpauldoDto.cs
+++ Models/SpauldoDto.cs
@@ -5,6 +5,10 @@ namespace spauldo_tecture.Models
     public abstract class SpauldoDto : IDto
     {
         public virtual string Id { get; set; }
+        public virtual int? Decode(IHashids hashids)
+        {
+            return String.IsNullOrEmpty(this.Id) ? default(int) : hashids.Decode(this.Id).FirstOrDefault();
+        }
         public abstract IModel MapToModel(IHashids hashids);
     }
 }
